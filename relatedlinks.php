83786a68b1209c978c169c4ccd5965e66a4bcf0c
AlexKul <alex.kulakevich@gmail.com>
2014-10-22 19:57:05 -0300

    Added Related Links page.

---
+++ relatedlinks.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<?php include('head.php') ?>
+	<body>
+		<?php $relatedlinks = true ?>
+		<?php include('navbar.php') ?>
+
+		<!-- Begin page content -->
+		<div class="container">
+			<div class="page-header">
+				<h1>Related Links</h1>
+			</div>
+
+			<h3>Local Women in Technology:</h3>
+
+			<ul>
+				<li>
+					<a href="http://www.catawit.ca/chapters/atlantic/">Canadian Women in Technology-Atlantic Chapter</a>
+				</li>
+				<li>
+					<a href="http://www.ccwestt.org/">Canadian Coalition of Women in Engineering, Science, Trades and Technology</a>
+				</li>	
+			</ul>
+
+
+			<h3>Global Women in Technology:</h3>
+
+			<ul>
+				<li>
+					<a href="http://women.acm.org/">ACM's Women in Computing</a>
+				</li>
+				<li>
+					<a href="http://gracehopper.org">Grace Hopper Celebration of Women in Computing</a>
+				</li>	
+				<li>
+					<a href="http://www.ieee.org/membership_services/membership/women/index.html">IEEE Women in Engineering</a>
+				</li>
+			</ul>
+
+
+
+
+			<h3>Opportunities for Female IT Students:</h3>
+
+			<ul>
+				<li>
+					<a href="http://www.google.ca/anitaborg/">Google Canada Anita Borg Memorial Scholarship</a>
+				</li>
+				<li>
+					<a href="http://www.google.com/jobs/engpracticum/">Google Engineering/Computer Science Practicum</a>
+				</li>	
+			</ul>
+
+
+
+			<h3>Scholarships for Females at Dalhousie (CS and INFX):</h3>
+
+			<ul>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/ada-byron-award">Ada Byron Award</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/citizenship-award/">Citizenship Award</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/google-canada-anita-borg-memorial-scholarship">Google Canada Anita Borg Memorial Scholarship</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/leadership-award">Leadership Award</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/eliza-ritchie-doctoral-scholarship-women">Eliza Ritchie Doctoral Scholarship</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/nrc-women-science-and-engineering">NRC Women in Science and Engineering</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/gold-award">Gold Award</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/silver-award">Silver Award</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/bronze-award">Bronze Award</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/deans-list">Dean's List</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships/sexton-scholar">Sexton Scholar</a>
+				</li>
+				<li>
+					<a href="http://www.cs.dal.ca/scholarships">For all other scholarships offered in Dal CS or INFX</a>
+				</li>
+
+			</ul>
+					
+			<h3>Free Lectures and Online Courses :)</h3>
+
+			<ul>
+
+				<li>
+					<a href="http://www.onlinecourses.com/">Online Courses (free lectures online from top universities)</a>
+				</li>
+
+				<li>
+					<a href="http://www.onlineuniversity.net/">Online University</a>
+				</li>
+
+			</ul>
+					
+			
+		</div>
+
+		<?php include('footer.php') ?>
+		<?php include('javascript.php') ?>
+	</body>
+</html>
